6a363d1f6923b1ac28c09790066d4282b1315779
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-12 23:33:18 -0500

    findUsersByUsername completed

--- find-findindex.js
+++ find-findindex.js
@@ -11,8 +11,11 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
-
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find((val) => {
+    return val.username === username;
+  });
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
